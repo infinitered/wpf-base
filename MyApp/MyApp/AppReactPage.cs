3f851bae2c8fc14fca5a03eb3f5ed3e2c8393c9b
Gant Laborde <gantman@gmail.com>
2016-12-30 10:50:38 -0800

    initial commit

---
+++ MyApp/MyApp/AppReactPage.cs
@@ -0,0 +1,41 @@
+﻿using ReactNative;
+using ReactNative.Modules.Core;
+using ReactNative.Shell;
+using System.Collections.Generic;
+
+namespace blue
+{
+    internal class AppReactPage : ReactPage
+    {
+        public override string MainComponentName => "blue";
+
+        //public override string JavaScriptMainModuleName => "../../index.windows";
+
+#if BUNDLE
+        public override string JavaScriptBundleFile
+        {
+            get
+            {
+                return "ms-appx:///ReactAssets/index.windows.bundle";
+            }
+        }
+#endif
+
+        public override List<IReactPackage> Packages => new List<IReactPackage>
+        {
+            new MainReactPackage(),
+        };
+
+        public override bool UseDeveloperSupport
+        {
+            get
+            {
+#if !BUNDLE || DEBUG
+                return true;
+#else
+                return false;
+#endif
+            }
+        }
+    }
+}
